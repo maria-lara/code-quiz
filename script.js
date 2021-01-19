9599085beccfd6e12cbf41c374507ff42a2dd19c
Maria Lara <74436232+maria-lara@users.noreply.github.com>
2021-01-18 17:28:53 -0800

    add js variables and quiz questions

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-script.js
+// Variables from HTML //
+
+const questionDisplay = document.querySelectorAll('questions');
+const submitButton = docuemtn.querySelectorAll('submit');
+const scoreDislay = document.querySelectorAll('score');
+
+
+// Quiz questions //
+
+const quizQuestions = [
+    { 
+        question: "Question 1",
+        answers: {a: 'x', b: 'y', c:'z'},
+        correctAnswer: 'b'
+    },
+
+    { 
+        question: "Question 2",
+        answers: {a: 'x', b: 'y', c:'z'},
+        correctAnswer: 'b'
+    },
+
+    { 
+        question: "Question 3",
+        answers: {a: 'x', b: 'y', c:'z'},
+        correctAnswer: 'b'
+    },
+
+    { 
+        question: "Question 4",
+        answers: {a: 'x', b: 'y', c:'z'},
+        correctAnswer: 'b'
+    },
+
+    { 
+        question: "Question 5",
+        answers: {a: 'x', b: 'y', c:'z'},
+        correctAnswer: 'b'
+    },
+
+    { 
+        question: "Question 6",
+        answers: {a: 'x', b: 'y', c:'z'},
+        correctAnswer: 'b'
+    },
+
+    { 
+        question: "Question 7",
+        answers: {a: 'x', b: 'y', c:'z'},
+        correctAnswer: 'b'
+    },
+
+];
+
